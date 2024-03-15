d898572b510b9e2ac87938a2b5b9185a8001c3a4
DEAFG0AT <de4fg0at@gmail.com>
2024-03-14 18:53:57 -0700

    Initial commit

---
+++ src/main/java/net/motley/motcraft/motcraftModClient.java
@@ -0,0 +1,11 @@
+package net.motley.motcraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class motcraftModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
